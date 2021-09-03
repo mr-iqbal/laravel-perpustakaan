ae35c10143d36efe95fbda1826623e62e26d55bb
iqbalrasetio16@gmail.com <mr-iqbal>
2021-09-03 10:50:19 +0700

    init commit

---
+++ resources/views/child.blade.php
@@ -0,0 +1,14 @@
+@extends('template.master')
+
+
+@section('content')
+    <div class="card">
+        <div class="card-header">
+            <h4>Judul Modul</h4>
+        </div>
+        <div class="card-body">
+            <h4>Halaman Kosong</h4>
+            <img src="img/kosong.jpg" alt="" style="width:500px; height:320px;">
+        </div>
+    </div>
+@endsection
